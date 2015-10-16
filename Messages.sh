4e54978448dc610fe1ea0279555097590fdcafdf
Yuri Chornoivan <yurchor@ukr.net>
2015-10-16 19:28:56 +0300

    Do not extract every single string in the code into translation catalog

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 find . -name "*.provider.in" -o -name "*.service.in" -type f | xargs -n1 intltool-extract -l -type="gettext/xml"
-$XGETTEXT `find . -name "*.h" -o -name "*.cpp"` -c -a -o $podir/kaccounts-mobile.pot
+$XGETTEXT `find . -name "*.h" -o -name "*.cpp"` -kN_ -c -o $podir/kaccounts-mobile.pot
 rm -rf ./tmp/
